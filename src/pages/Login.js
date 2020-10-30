ab2d34533bc81f34a63594add8d26ab741e3bd56
Ricardo <ricamposm@gmail.com>
2020-10-29 18:25:08 -0600

    styled facebook button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,18 +7,22 @@ const responseFacebook = (response) => {
   console.log(response)
 }
 
+const componentClicked = (e) => {
+  console.log(e)
+}
+
 export const Login = () => {
   return (
-    <Container className="main-cont">
+    <Container>
       <Grid container justify="space-between" className="cont">
         <div>
           <h2 className="title">Welcome to MovieBucket</h2>
           <p className="desc">Here you can look for the movies you want to see and get a description, rating, <br/>watch the trailer and more. Please log in with Facebook to continue</p>
           <FacebookLogin 
             appId="1396011717456903"
-            autoLoad={true}
             fields="name,email,picture"
             callback={responseFacebook}
+            cssClass="facebookButton"
           />
         </div>
         <img className="cinemaSVG" src='/img/cinema.svg' alt="cinema SVG"/>
